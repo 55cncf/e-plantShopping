dfbb4ffafd3ad253736623115acb1ad82fd76751
55cncf <98778831+55cncf@users.noreply.github.com>
2025-10-06 06:39:30 +0300

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,10 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './CartSlice';
-const store = configureStore({
+import { configureStore } from '@reduxjs/toolkit'
+import cartReducer from './CartSlice.jsx'
 
-    reducer: {
-        cart: cartReducer,
-    },
-});
+export const store = configureStore({
+  reducer: {
+    cart: cartReducer,
+  },
+})
 
-export default store;
+export default store
